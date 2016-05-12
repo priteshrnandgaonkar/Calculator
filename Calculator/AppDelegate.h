f2c2267788738d451dafe220f912f34abefc121c
prit91 <pncooldude91@gmail.com>
2016-05-12 16:37:12 +0530

    Initial Commit

---
+++ Calculator/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Calculator
+//
+//  Created by Pritesh Nandgaonkar on 12/05/16.
+//  Copyright © 2016 Pritesh Nandgaonkar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Calculator/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Calculator
+//
+//  Created by Pritesh Nandgaonkar on 12/05/16.
+//  Copyright © 2016 Pritesh Nandgaonkar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
